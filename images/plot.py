1bb97ca233df2d26dc589e7402f63c2060feb8c7
Yaqi Zhang <zhang623@wisc.edu>
2018-04-20 14:11:26 -0500

    update new plot

--- images/plot.py
+++ images/plot.py
@@ -9,20 +9,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("seaborn")
-# import seaborn
-# seaborn.set()
+import seaborn
+seaborn.set()
 
 
 if __name__ == "__main__":
-    i7_2600_times = [5.014, 3.405, 5.860, 62.517]
-    e5_times = [3.012, 2.632, 4.887, 55.174]
-    i7_6700_times = [2.403, 13.615, 4.032, 60.159]
-    xticks = ['C', 'Fortran', 'Julia', 'Python']
+    xticks = ['Julia', 'C', 'Fortran', 'Java', 'Python', 'Javascript', 'R']
+    i7_4870HQ_times = np.log2(np.array([4.780, 3.693, 2.591, 3.628, 32.015, 16.445, 130.351]))
+    i7_2600_times = np.log2(np.array([7.093, 5.266, 3.569, 10.458, 65.815, 144.833, 224.067]))
+    e5_times = np.log2(np.array([5.505, 3.079, 2.650, 6.076, 56.443, 8.131, 124.805]))
+    i7_6700_times = np.log2(np.array([4.308, 2.370, 13.610, 4.122, 71.302, 6.452, 202.943]))
     n = len(xticks)
+    ax = plt.axes()
+    plt.scatter(np.arange(n), i7_4870HQ_times, color="orange", label="i7-4870HQ")
     plt.scatter(np.arange(n), i7_2600_times, color="red", label="i7-2600K")
     plt.scatter(np.arange(n), e5_times, color='blue', label="E5-2698")
     plt.scatter(np.arange(n), i7_6700_times, color="green", label="i7-6700")
     plt.xticks(np.arange(n), xticks)
+    ax.set_yscale('log')
     plt.ylabel('time (s)')
     plt.legend(loc="upper left")
     plt.show()
