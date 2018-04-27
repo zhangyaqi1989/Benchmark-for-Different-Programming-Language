f331e8d1ab9d8ecc1a28b64e6b5aef8ca0ea0dc9
Yaqi Zhang <zhang623@wisc.edu>
2018-04-27 17:50:48 -0500

    update

--- images/plot.py
+++ images/plot.py
@@ -16,9 +16,9 @@ seaborn.set()
 if __name__ == "__main__":
     xticks = ['Julia', 'C', 'C++', 'Fortran', 'Java', 'CPython', 'NodeJS', 'R']
     i7_4870HQ_times = np.log2(np.array([4.780, 3.693, 3.389, 2.591, 3.628, 32.015, 16.445, 130.351]))
-    i7_2600_times = np.log2(np.array([7.093, 5.266, 3.569, 10.458, 65.815, 144.833, 224.067]))
-    e5_times = np.log2(np.array([5.505, 3.079, 2.650, 6.076, 56.443, 8.131, 124.805]))
-    i7_6700_times = np.log2(np.array([4.308, 2.370, 13.610, 4.122, 71.302, 6.452, 202.943]))
+    i7_2600_times = np.log2(np.array([7.093, 5.266, 5.010, 3.569, 10.458, 65.815, 144.833, 224.067]))
+    e5_times = np.log2(np.array([5.505, 3.079, 3.033, 2.650, 6.076, 56.443, 8.131, 124.805]))
+    i7_6700_times = np.log2(np.array([4.308, 2.370, 2.330, 13.610, 4.122, 71.302, 6.452, 202.943]))
     n = len(xticks)
     ax = plt.axes()
     plt.scatter(np.arange(n), i7_4870HQ_times, color="orange", label="i7-4870HQ")
